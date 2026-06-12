d5be5c8dc9e2dd3cd7bc83f21eda631e1e344634
Kishore143kk <kishoresundarrajan007@gmail.com>
2026-06-12 12:13:15 +0530

    Fix Render deployment

--- server.ts
+++ server.ts
@@ -79,7 +79,7 @@ if (productCount.count < 70) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
